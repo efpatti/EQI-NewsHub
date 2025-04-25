3323b5bb4139b6e0ead3ff47f9dabc87290b2b91
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-25 15:17:23 -0300

    chore: initialize project structure with Spring Boot and Next.js
    Set up backend with Spring Boot 3.4.5 and PostgreSQL, and frontend using Next.js.
    Initial folder structure, config files and dependencies are in place.

---
+++ eqi-news-hub/src/main/java/br/com/eqimarket/eqi_news_hub/EqiNewsHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.eqimarket.eqi_news_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqiNewsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqiNewsHubApplication.class, args);
+	}
+
+}
---
+++ eqi-news-hub/src/test/java/br/com/eqimarket/eqi_news_hub/EqiNewsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eqimarket.eqi_news_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqiNewsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
